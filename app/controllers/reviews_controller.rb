3f59d7e0c7961b442d98926eb8b1f87f8e7d465d
Lela Smith <smith.lelaa@gmail.com>
2020-01-17 12:50:52 -0800

    add reviews controller code

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,4 +6,46 @@ class ReviewsController < ApplicationController
     render :new
   end
 
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to product_path(@review.product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@review.product)
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:author, :content_body, :rating)
+  end
+
 end

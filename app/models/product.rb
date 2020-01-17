45fd6be15dabfc7e9746408e28293cf5270d17c3
Lela Smith <smith.lelaa@gmail.com>
2020-01-17 15:30:03 -0800

    add validations for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :cost, presence: true
+  validates :cost, numericality: true
+  validates :country_of_origin, presence: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 describe Product do
   it { should have_many(:reviews) }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :cost }
+  it { should validate_numericality_of :cost }
+  it { should validate_presence_of :country_of_origin }
 end

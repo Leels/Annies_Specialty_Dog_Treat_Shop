035c6c005ec83fbc25311b186a91f65f57f42aaf
Lela Smith <smith.lelaa@gmail.com>
2020-01-17 15:41:02 -0800

    add validations to reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, presence: true
+  validates :rating, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates_length_of :content_body, minimum: 50, maximum: 250
 end

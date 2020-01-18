2e93cc2882db59b4be50bbfdf4cc9885f571f89f
Lela Smith <smith.lelaa@gmail.com>
2020-01-17 16:00:30 -0800

    add callbacks to titleize product name and country of origin

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,11 @@ class Product < ApplicationRecord
   validates :cost, presence: true
   validates :cost, numericality: true
   validates :country_of_origin, presence: true
+  before_save(:titleize_product)
+
+  private
+    def titleize_product
+      self.name = self.name.titleize
+      self.country_of_origin = self.country_of_origin.titleize
+    end
 end

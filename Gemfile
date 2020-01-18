399dd9c41a6dc1294d34151bd1a291bb51e5f702
Lela Smith <smith.lelaa@gmail.com>
2020-01-17 16:56:59 -0800

    add faker seed to product

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ gem 'bootstrap-sass', '~> 3.4.1'
 
 gem 'sassc-rails', '>= 2.1.0'
 
+gem 'faker'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+50.times do |index|
+  Product.create!(name: Faker::Food.dish, cost: Faker::Number.decimal(l_digits: 2), country_of_origin: Faker::Nation.nationality)
+end
+
+p "Created #{Product.count} products"

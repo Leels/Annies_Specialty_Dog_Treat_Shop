0ce44a176faffbcdd415b30e1b103dab3fb914e5
Lela Smith <smith.lelaa@gmail.com>
2020-01-17 16:23:45 -0800

    add flash messages to product and review

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,6 +13,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
+      flash[:notice] = "Product successfully added!"
       redirect_to products_path
     else
       render :new
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,6 +10,7 @@ class ReviewsController < ApplicationController
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new(review_params)
     if @review.save
+      flash[:notice] = "Review successfully added!"
       redirect_to product_path(@product)
     else
       render :new

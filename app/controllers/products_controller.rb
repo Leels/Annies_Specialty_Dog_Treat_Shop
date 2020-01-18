9a129b9d4bdd4123fb7cac24d56523e4100059cf
Lela Smith <smith.lelaa@gmail.com>
2020-01-18 15:01:18 -0800

    add html landing page and scopes to project

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,6 +16,7 @@ class ProductsController < ApplicationController
       flash[:notice] = "Product successfully added!"
       redirect_to products_path
     else
+      flash[:alert] = "Product not added, see error notes below"
       render :new
     end
   end
@@ -33,8 +34,10 @@ class ProductsController < ApplicationController
   def update
     @product= Product.find(params[:id])
     if @product.update(product_params)
+      flash[:notice] = "Product successfully updated"
       redirect_to products_path
     else
+       flash[:alert] = "Product not updated, see error notes below"
       render :edit
     end
   end
@@ -42,6 +45,7 @@ class ProductsController < ApplicationController
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
+     flash[:notice] = "Product successfully removed"
     redirect_to products_path
   end
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,25 @@ class Product < ApplicationRecord
   validates :country_of_origin, presence: true
   before_save(:titleize_product)
 
+  scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
+
+  scope :most_reviews, -> {(
+    select("products.id, products.name, products.cost, products.coountry_of_origin, products.img, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(10)
+    )}
+
+  scope :alphabetize, -> {(
+    select("products.name, products.id")
+    .order("products.name")
+  )}
+
+  scope :find_usa_product, -> (country_parameter) {
+   where(:country_of_origin => country_parameter)
+ }
+
   private
     def titleize_product
       self.name = self.name.titleize

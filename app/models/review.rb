031ee59023d0e6f72012690a7812f91d4f12604b
Lela Smith <smith.lelaa@gmail.com>
2020-01-20 22:43:10 -0800

    fix error messages

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,7 +5,6 @@ class Review < ApplicationRecord
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
   validates_length_of :content_body, minimum: 50, maximum: 250
 
-
   def blank_stars
     5 - rating.to_i
   end

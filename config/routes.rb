841e33277dd67504d56e81d985dad2825355d38a
Lela Smith <smith.lelaa@gmail.com>
2020-01-24 10:21:41 -0800

    add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root to: 'landing#index'
+
+  get '/signup' => 'users#new'
+post '/users' => 'users#create'
+
+get '/signin' => 'sessions#new'
+post '/signin' => 'sessions#create'
+get '/signout' => 'sessions#destroy'
+
   resources :products do
     resources :reviews
   end

6a5c76b9b4955e1f20edf45b7467bb7d4d61dde8
Lela Smith <smith.lelaa@gmail.com>
2020-01-26 16:22:54 -0800

    add integration tests for session

---
+++ spec/features/session_pages_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'the session sign-in process', :logged_out do
+    it "begins session" do
+        User.create!({username: 'CapyTest', email: 'capytest@test.com', password: 'password'})
+        visit '/'
+        click_on 'Sign in'
+        fill_in 'email', :with => 'capytest@test.com'
+        fill_in 'password', :with => 'password'
+        click_on 'Sign In'
+        expect(page).to have_content 'You\'ve signed in.'
+    end
+    it "gives error when credentials are incorrect" do
+        visit '/'
+        click_on 'Sign in'
+        fill_in 'email', :with => 'test@test.com'
+        fill_in 'password', :with => ''
+        click_on 'Sign In'
+        expect(page).to have_content 'There was a problem signing in. Please try again.'
+    end
+    it "ends session" do
+        User.create!({username: 'CapyTest', email: 'capytest@test.com', password: 'password'})
+        visit '/'
+        click_on 'Sign in'
+        fill_in 'email', :with => 'capytest@test.com'
+        fill_in 'password', :with => 'password'
+        click_on 'Sign In'
+        click_on 'Sign out'
+        expect(page).to have_content 'You\'ve signed out.'
+    end
+end
